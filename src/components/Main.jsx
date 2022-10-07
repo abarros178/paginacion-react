1a3a98a764a4a024c9fd2f15b7944a83e4a90ab7
abarros17 <abarros17@cuc.edu.co>
2022-10-07 17:08:26 -0500

    retoques

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,10 @@ import { getMoviesPage } from '../api/axios.js'
 import { useState } from 'react'
 import PageButton from './PageButton'
 import Movie from './Movie'
-import { Grid } from '@mui/material'
+import { CircularProgress, Grid, IconButton } from '@mui/material'
+// import '../Css/Main.css'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const Main = () => {
     const [page, setPage] = useState(1)
@@ -24,30 +27,40 @@ const Main = () => {
     if (isError) return <p>Error: {error.message}</p>
     // movies.results.map(movie => console.log(movie))
     
-    const content = movies.results.map(movie => <Movie key={movie.id} movie={movie} />)
+    const content = movies.results.map(movie => <Grid item xs={3}> <Movie key={movie.id} movie={movie} /></Grid>)
 
-    const lastPage = () => setPage(page+1)
+    const lastPage = () => setPage(page-1)
 
-    const firstPage = () => setPage(page-1)
+    const firstPage = () => setPage(page+1)
 
     const pagesArray = Array(movies.total_pages).fill().map((_, index) => index + 1)
 
     const nav = (
-        <nav className="nav-ex2">
-            <button onClick={firstPage} disabled={isPreviousData || page === 1}>&lt;&lt;</button>
+            <Grid>
+            {/* <button onClick={firstPage} center disabled={isPreviousData || page === 1}>&lt;&lt;</button> */}
+            <IconButton onClick={firstPage} center  color="secondary" aria-label="add an alarm">
+                <ArrowForwardIcon />
+            </IconButton>
             {/* Removed isPreviousData from PageButton to keep button focus color instead */}
             {/* {pagesArray.map(pg => <PageButton key={pg} pg={pg} setPage={setPage} />)} */}
-            <button onClick={lastPage} disabled={isPreviousData || page === movies.total_pages}>&gt;&gt;</button>
-        </nav>
+            {/* <button onClick={lastPage} disabled={isPreviousData || page === movies.total_pages}>&gt;&gt;</button> */}
+            <IconButton onClick={lastPage} disabled={page===1} color="secondary" aria-label="previus">
+                <ArrowBackIcon />
+                </IconButton>
+                </Grid>
     )
 
     return (
         <>
             {nav}
-            {isFetching && <div class="ldBar"></div>}
-            <Grid container>
+            {
+            isFetching ? <Grid center> <CircularProgress /></Grid>:
+                <>
+            <Grid container spacing={1}>
             {content}
             </Grid>
+            </>
+            }
         </>
     )
 }
